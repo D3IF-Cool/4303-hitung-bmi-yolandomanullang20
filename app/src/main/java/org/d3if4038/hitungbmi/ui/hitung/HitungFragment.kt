38e77c558358f7abf79713042b40a872d9bcbcdb
Yolando Asri Erbenca Gegeh <yolandomanullang@student.telkomuniversity.ac.id>
2021-04-26 21:44:03 +0700

    3.2.Memperbaiki navigasi dengan view model

--- app/src/main/java/org/d3if4038/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4038/hitungbmi/ui/hitung/HitungFragment.kt
@@ -1,4 +1,4 @@
-package org.d3if4038.hitungbmi.ui
+package org.d3if4038.hitungbmi.ui.hitung
 
 import android.content.Intent
 import android.os.Bundle
@@ -18,7 +18,6 @@ import org.d3if4038.hitungbmi.databinding.FragmentHitungBinding
 class HitungFragment : Fragment() {
     private val viewModel : HitungViewModel by viewModels()
     private lateinit var binding: FragmentHitungBinding
-    private lateinit var kategoriBmi: KategoriBmi
 
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -43,10 +42,8 @@ class HitungFragment : Fragment() {
 
         binding.button.setOnClickListener { hitungBmi() }
         binding.buttonReset.setOnClickListener { resetForm() }
-        binding.saranButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(
-                HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi)
-            )
+        binding.saranButton.setOnClickListener{
+            viewModel.mulaiNavigasi()
         }
         binding.shareButton.setOnClickListener { shareData() }
         setHasOptionsMenu(true)
@@ -54,6 +51,12 @@ class HitungFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.getNavigasi().observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            findNavController().navigate(HitungFragmentDirections
+                .actionHitungFragmentToSaranFragment(it))
+            viewModel.selesaiNavigasi()
+        })
         viewModel.getHasilBmi().observe(viewLifecycleOwner, {
             if (it == null) return@observe
             binding.bmiTextView.text = getString(R.string.bmi_x, it.bmi)
